6f8cf85f4e7ce883f7fd0dd1c1db71d881add772
Дмитро Катюха <Dmytro.Katyukha@jbm.com.ua>
2017-01-11 14:34:37 +0200

    Fix install scripts

--- install-user.bash
+++ install-user.bash
@@ -31,7 +31,7 @@ ODOO_HELPER_BIN=${ODOO_HELPER_BIN:-$INSTALL_PATH/bin};
 # clone repo
 if [ ! -d $INSTALL_PATH ]; then
     git clone -q https://github.com/katyukha/odoo-helper-scripts $INSTALL_PATH;
-    git checkout -q $ODOO_HELPER_BRANCH;
+    (cd $INSTALL_PATH && git checkout -q $ODOO_HELPER_BRANCH);
     # TODO: may be it is good idea to pull changes from repository if it is already exists?
     # TODO: implement here some sort of upgrade mechanism?
 fi
