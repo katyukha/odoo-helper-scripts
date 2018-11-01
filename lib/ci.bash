bec5ccb01a116d16e2c84acbae5a329799fdb17b
Dmytro Katyukha <cto@crnd.pro>
2018-11-01 15:01:28 +0200

    odoo-helper ci: optionaly check repository version change

--- lib/ci.bash
+++ lib/ci.bash
@@ -23,6 +23,91 @@ ohelper_require 'utils';
 
 set -e; # fail on errors
 
+# ci_ensure_versions_ok <version1> <version2>
+# ensure that version2 is greater than version1
+function ci_ensure_versions_ok {
+    local version1="$1"; shift;
+    local version2="$1"; shift;
+
+    # NOTE: here we inverse pythons True to bash zero status code
+    if ! run_python_cmd "from pkg_resources import parse_version as V; exit(V('${version1}') < V('${version2}'));"; then
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+# ci_validate_version <version>
+# validate specified version
+# version must look like x.x.y.y.y
+# where x.x is odoo version and y.y.y repository version
+function ci_validate_version {
+    local version="$1";
+    if ! [[ "$version" =~ ^${ODOO_VERSION}.[0-9]+.[0-9]+.[0-9]+ ]]; then
+        return 1;
+    fi
+    return 0;
+}
+
+# ci_git_get_repo_version_by_ref [-q] <repo path> <ref>
+function ci_git_get_repo_version_by_ref {
+    if [ "$1" == "-q" ]; then
+        local quiet=1;
+        shift;
+    fi
+
+    local repo_path="$1"; shift;
+    local git_ref="$1"; shift;
+    local cdir="$(pwd)";
+    local version;
+
+    cd "$repo_path";
+    version=$(git show -q ${git_ref}:./VERSION 2>/dev/null);
+    if [ $? -ne 0 ] || [ -z "$version" ]; then
+        [ -z "$quiet" ] && echoe -e "${YELLOWC}WARNING${NC}: repository version file (${BLUEC}${repo_path}/VERSION${NC}) not found (revision ${BLUEC}${git_ref}${NC}! Using default version ${BLUEC}${ODOO_VERSION}.0.0.0${NC}";
+        echo "${ODOO_VERSION}.0.0.0"
+        return 1;
+    else
+        echo "$version";
+    fi
+
+    cd "$cdir";
+}
+
+# ci_git_get_addon_version_by_ref [-q] <addon path> <ref>
+function ci_git_get_addon_version_by_ref {
+    if [ "$1" == "-q" ]; then
+        local quiet=1;
+        shift;
+    fi
+
+    local addon_path="$1"; shift;
+    local git_ref="$1"; shift;
+    local cdir="$(pwd)";
+
+    local manifest_content;
+    local version="${ODOO_VERSION}.0.0.0";
+
+    cd "$addon_path";
+
+    # Get manifest content at start revision
+    local manifest_content="$(git show -q ${git_ref}:./__manifest__.py 2>/dev/null)";
+    if [ -z "$manifest_content" ]; then
+        local manifest_content="$(git show -q ${git_ref}:./__openerp__.py 2>/dev/null)";
+    fi
+
+    # Get version in first revision
+    if [ -z "$manifest_content" ]; then
+        local version="${ODOO_VERSION}.0.0.0";
+    else
+        local version=$(echo "$manifest_content" | execv python -c "\"import sys; print(eval(sys.stdin.read()).get('version', '${ODOO_VERSION}.1.0.0'))\"");
+        if [ $? -ne 0 ]; then
+            [ -z "$quiet" ] && echoe -e "${YELLOWC}WARNING${NC} Cannot read version from manifest in first revision! Using ${BLUEC}${ODOO_VERSION}.0.0.0${NC} as default.";
+        fi
+    fi
+    echo "$version";
+    cd "$cdir";
+}
 
 # ci_check_versions_git <repo> <ref start> <ref end>
 function ci_check_versions_git {
@@ -35,6 +120,14 @@ function ci_check_versions_git {
     Options:
         --ignore-trans  - ignore translations
                           Note: this option may not work on old git versions
+        --repo-version  - ensure repository version updated.
+                          Repository version have to be specified in
+                          file named VERSION placed in repository root.
+                          Version have to be string of
+                          5 numbers separated by dots.
+                          For example: 11.0.1.0.0
+                          Version number have to be updated if at least one
+                          addon changed
         -h|--help|help  - print this help message end exit
 
     Parametrs:
@@ -61,17 +154,22 @@ function ci_check_versions_git {
                 git_changed_extra_opts="$git_changed_extra_opts --ignore-trans";
                 shift;
             ;;
+            --repo-version)
+                check_repo_version=1;
+                shift;
+            ;;
             *)
                 break;
             ;;
         esac
     done
 
-    local repo_path="$1"; shift;
+    local repo_path=$(readlink -f "$1"); shift;
     local ref_start="$1"; shift;
     local ref_end="$1"; shift;
     local cdir="$(pwd)";
 
+    # Check addons versions
     local changed_addons=( $(git_get_addons_changed $git_changed_extra_opts "$repo_path" "$ref_start" "$ref_end") );
     local result=0;
     for addon_path in "${changed_addons[@]}"; do
@@ -79,66 +177,47 @@ function ci_check_versions_git {
         local addon_name=$(basename "$addon_path");
 
         echoe -e "${BLUEC}Checking version of ${YELLOWC}${addon_name}${BLUEC} addon ...${NC}";
+        local version_before=$(ci_git_get_addon_version_by_ref -q "$addon_path" "${ref_start}");
+        local version_after=$(ci_git_get_addon_version_by_ref "$addon_path" "${ref_end}");
 
-        # Get manifest content at start revision
-        local manifest_content_before="$(git show -q ${ref_start}:./__manifest__.py 2>/dev/null)";
-        if [ -z "$manifest_content_before" ]; then
-            local manifest_content_before="$(git show -q ${ref_start}:./__openerp__.py 2>/dev/null)";
-        fi
-
-        # Get manifest content at and revision
-        local manifest_content_after="$(git show -q ${ref_end}:./__manifest__.py 2>/dev/null)";
-        if [ -z "$manifest_content_after" ]; then
-            local manifest_content_after="$(git show -q ${ref_end}:./__openerp__.py 2>/dev/null)";
-        fi
-
-        # Get version in first revision
-        if [ -z "$manifest_content_before" ]; then
-            local version_before="${ODOO_VERSION}.0.0.0";
-        else
-            local file_name="/tmp/oh-ci-vc-git-before-$(random_string)";
-            echo "$manifest_content_before" > $file_name;
-            local version_before=$(run_python_cmd "print(eval(open('$file_name', 'rt').read()).get('version', '${ODOO_VERSION}.1.0.0'))");
-            if [ $? -ne 0 ]; then
-                echoe -e "${YELLOWC}WARNING${NC} Cannot read version from manifest in first revision! Using ${BLUEC}${ODOO_VERSION}.0.0.0${NC} as default.";
-            fi
-            rm -f "$file_name";
-        fi
-
-        # Get version in second revision
-        if [ -z "$manifest_content_after" ]; then
-            echoe -e "${YELLOWC}WARNING${NC} cannot find manifest in second revision. it seems that it was removed.";
-            continue
-        fi
-        local file_name="/tmp/oh-ci-vc-git-after-$(random_string)";
-        echo "$manifest_content_after" > $file_name;
-        local version_after=$(run_python_cmd "print(eval(open('$file_name', 'rt').read()).get('version', '${ODOO_VERSION}.1.0.0'))");
-        if [ $? -ne 0 ]; then
-            echoe -e "${REDC}ERROR${NC} Cannot read version from manifest in second revision! It seems that manifest is broken!";
-            result=1;
-            rm -f "$file_name";
-            continue
-        fi
-        rm -f "$file_name";
-
-        if ! [[ "$version_after" =~ ^${ODOO_VERSION}.[0-9]+.[0-9]+.[0-9]+ ]]; then
+        if ! ci_validate_version "$version_after"; then
             echoe -e "${REDC}FAIL${NC}: Wrong version format. Correct version must match format: ${YELLOWC}${ODOO_VERSION}.X.Y.Z${NC}. Got ${YELLOWC}${version_after}${NC}";
             result=1;
             continue;
         fi
 
         # Compare version
-        # NOTE: here we inverse pythons True to bash zero status code
-        if ! run_python_cmd "from pkg_resources import parse_version as V; exit(V('${version_before}') < V('${version_after}'));"; then
+        if ci_ensure_versions_ok "$version_before" "$version_after"; then
             # version before is less that version_after
             echoe -e "${GREENC}OK${NC}";
         else
-            echoe -e "${REDC}FAIL${NC}: incorrect new version!"
+            echoe -e "${REDC}FAIL${NC}: ${YELLOWC}${addon_name}${NC} have incorrect new version!"
             echoe -e "${BLUEC}-----${NC} new version ${YELLOWC}${version_after}${NC} must be greater than old version ${YELLOWC}${version_before}${NC}";
             result=1;
         fi
 
     done;
+
+    # Check repo version
+    if [ "$check_repo_version" -eq 1 ]; then
+        local repo_version_before=$(ci_git_get_repo_version_by_ref -q "$repo_path" "$ref_start");
+        local repo_version_after=$(ci_git_get_repo_version_by_ref "$repo_path" "$ref_end");
+        if [ -z "$repo_version_after" ]; then
+            echoe -e "${REDC}ERROR${NC}: repository version not specified! Please, specify repository version in ${YELLOWC}${repo_path}/VERSION${NC} file.";
+            return 2;
+        fi
+        if ! ci_validate_version "$repo_version_after"; then
+            echoe -e "${REDC}ERROR${NC}: Wrong repo version format. Correct version must match format: ${YELLOWC}${ODOO_VERSION}.X.Y.Z${NC}. Got ${YELLOWC}${repo_version_after}${NC}"
+            return 2;
+        fi
+        if ci_ensure_versions_ok "$repo_version_before" "$repo_version_after"; then
+            echoe -e "Repository version: ${GREENC}OK${NC}";
+        else
+            echoe -e "${REDC}FAIL${NC}: incorrect new repository version!"
+            echoe -e "${BLUEC}-----${NC} new reposotory version ${YELLOWC}${repo_version_after}${NC} must be greater than old version ${YELLOWC}${repo_version_before}${NC}";
+            result=1;
+        fi
+    fi
     cd "$cdir";
     return $result;
 }
@@ -200,9 +279,9 @@ function ci_command {
     NOTE: This command is experimental and everything may be changed.
 
     Usage:
-        $SCRIPT_NAME ci check-versions-git [--help]  - ensure versions of changed addons were updated
-        $SCRIPT_NAME ci ensure-icons <addon path>    - ensure all addons in specified directory have icons
-        $SCRIPT_NAME ci -h|--help|help               - show this help message
+        $SCRIPT_NAME ci check-versions-git [--help]     - ensure versions of changed addons were updated
+        $SCRIPT_NAME ci ensure-icons <addon path>       - ensure all addons in specified directory have icons
+        $SCRIPT_NAME ci -h|--help|help                  - show this help message
     ";
 
     if [[ $# -lt 1 ]]; then
